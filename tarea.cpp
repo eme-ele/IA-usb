044aae86f9946b03f5402d4fd19365d8057b10e7
ML <emele18@gmail.com>
2013-02-06 22:29:01 -0430

    Cambia estructura de entradas a vector<vect>

--- tarea.cpp
+++ tarea.cpp
@@ -6,54 +6,56 @@
 
 using namespace std;
 
-struct Pair {
-	double v1;
-	double v2;
-};
 
-typedef struct Pair Pair;
-
-int load_or(vector<Pair>& values, vector<double>& output){
-	Pair a,b,c,d;
-	a.v1 = 0; a.v2 = 0;
+int load_or(vector<vector<int> >& values, vector<double>& output){
+	vector<int> a(2);
+	vector<int> b(2);
+	vector<int> c(2);
+	vector<int> d(2);
+	a[0] = 0; a[1] = 0;
 	values[0] = a; output[0] = 0;
-	b.v1 = 0; b.v2 = 1;
+	b[0] = 0; b[1] = 1;
 	values[1] = b; output[1] = 1;
-	c.v1 = 1; c.v2 = 0;
+	c[0] = 1; c[1] = 0;
 	values[2] = c; output[2] = 1;
-	d.v1 = 1; d.v2 = 1;
+	d[0] = 1; d[1] = 1;
 	values[3] = d; output[3] = 1;
 	return 1;
 }
 
-int load_and(vector<Pair>& values, vector<double>& output){
-	Pair a,b,c,d;
-	a.v1 = 0; a.v2 = 0;
+int load_and(vector<vector<int> >& values, vector<double>& output){
+	vector<int> a(2);
+	vector<int> b(2);
+	vector<int> c(2);
+	vector<int> d(2);
+	a[0] = 0; a[1] = 0;
 	values[0] = a; output[0] = 0;	
-	b.v1 = 0; b.v2 = 1; 
+	b[0] = 0; b[1] = 1; 
 	values[1] = b; output[1] = 0;	
-	c.v1 = 1; c.v2 = 0;
+	c[0] = 1; c[1] = 0;
 	values[2] = c; output[2] = 0;
-	d.v1 = 1; d.v2 = 1;
+	d[0] = 1; d[1] = 1;
 	values[3] = d; output[3] = 1;
 	return 1;
 }
 
-int load_xor(vector<Pair>& values, vector<double>& output){
-	Pair a,b,c,d;
-	a.v1 = 0; a.v2 = 0;
+int load_xor(vector<vector<int> >& values, vector<double>& output){
+	vector<int> a(2);
+	vector<int> b(2);
+	vector<int> c(2);
+	vector<int> d(2);
+	a[0] = 0; a[1] = 0;
 	values[0] = a; output[0] = 0;
-	b.v1 = 0; b.v2 = 1;
+	b[0] = 0; b[1] = 1;
 	values[1] = b; output[1] = 1;
-	c.v1 = 1; c.v2 = 0;
+	c[0] = 1; c[1] = 0;
 	values[2] = c; output[2] = 1;
-	d.v1 = 1; d.v2 = 1;
+	d[0] = 1; d[1] = 1;
 	values[3] = d; output[3] = 0;
 	return 1;
 }
 
 int print(vector<double>& weights){
-	cout << "Res: ";
 	for (int i=0; i < weights.size(); i++){
 		cout << weights[i] << " ";
 	}
@@ -61,15 +63,14 @@ int print(vector<double>& weights){
 	return 1;
 }
 
-double dot_product(Pair value, vector<double>& weights){
+double dot_product(vector<int> value, vector<double>& weights){
 	double sum; 
-	sum = value.v1 * weights[0];
-	sum = sum + value.v2 * weights[1];
+	sum = value[0] * weights[0];
+	sum = sum + value[1] * weights[1];
 	return sum;
 }
 
-int perceptron(int n, double threshold, double learning_rate, vector<Pair>& values, vector<double>& output){
-	cout << "perceptron..." << endl;
+int perceptron(int n, double threshold, double learning_rate, vector<vector<int> >& values, vector<double>& output){
 	vector<double> weights(n);	
 	int error_count;
 	double error;
@@ -82,8 +83,9 @@ int perceptron(int n, double threshold, double learning_rate, vector<Pair>& valu
 			error = output[i] - result;
 			if (error != 0){
 				error_count++;
-				weights[0] = weights[0] + learning_rate * error * values[i].v1;
-				weights[1] = weights[1] + learning_rate * error * values[i].v2;
+				for(int j=0; j < weights.size(); j++){
+					weights[j] = weights[j] + learning_rate * error * values[i][j];
+				}
 			}
 		}
 		if (error_count == 0) {
@@ -102,60 +104,66 @@ double error_cuadrado(vector<double>& o, vector<double>& t){
 	
 }
 
-int adaline(int n, double learning_rate, vector<Pair>& values, vector<double>& output){
-
-	cout << "adeline..." << endl;
+int adaline(int n, double learning_rate, vector<vector<int> >& values, vector<double>& output){
 	vector<double> weights(n);
 	vector<double> grad_weights(n);
 	double error_count;
 	double error;
 	double min_error = 1000;
-	vector<double> result(2*n);
-	int x;
-	for(x=0; x<1000000;x++){
+	vector<double> result(values.size());
+	// store results
+	int num_it;
+	vector<double> final_weights(weights);
+	vector<double> final_result(result);
+	
+	for(int x=0; x<1000;x++){
 		error_count = 0;
 		for(int i=0; i < values.size(); i++){
 			//print(weights);
 			result[i] = dot_product(values[i], weights);
 			error = output[i] - result[i];
-			grad_weights[0] = grad_weights[0] + learning_rate * error * values[i].v1;
-			grad_weights[1] = grad_weights[1] + learning_rate * error * values[i].v2;
+			for(int j=0; j<grad_weights.size(); j++){
+				grad_weights[j] = grad_weights[j] + learning_rate * error * values[i][j];
+			}
 		}
-		weights[0] = weights[0] + grad_weights[0];
-		weights[1] = weights[1] + grad_weights[1];
+		for (int k=0; k<weights.size(); k++){
+			weights[k] = weights[k] + grad_weights[k];
+		}
 		error_count = error_cuadrado(result,output);
-		cout << "Error: " << error_count << endl;
-		//print(result);
-		min_error = min(error_count,min_error);
-		if (error_count < 0.166668) {
-			cout << "Iteraciones: " << x << " Error Minimo: " << min_error << endl;
-			return 1;
+	
+		// si encuentro un peso menor
+		if (error_count < min_error) {
+			num_it = x;
+			final_weights = weights;
+			min_error = error_count;
 		}
 	}
-	
+	cout << "Iteraciones: " << num_it << " Error Minimo: " << min_error << endl;
+	cout << "Pesos: ";
+	print(final_weights);
+	return 0;
 }
 
 int imprimir_uso(){
-	cerr << "Uso: ./tarea -[p | a] [num_entradas] -[or | and | xor]" << endl;
+	cerr << "Uso: ./tarea -[p | a] -[or | and | xor]" << endl;
 	return 1;
 }
 
 
 int main(int argc, char ** argv){
-	vector<Pair> values(4);
+	vector<vector<int> > values(4);
 	vector<double> output(4);
  
-	if (argc != 4) {
+	if (argc != 3) {
 		imprimir_uso();
 		return -1;
 	}
-	int n = atoi(argv[2]);
 
-	if (strcmp(argv[3],"-or")==0){
+	if (strcmp(argv[2],"-or")==0){
 		load_or(values,output);
-	} else if (strcmp(argv[3],"-and")==0) {
+	} else if (strcmp(argv[2],"-and")==0) {
 		load_and(values,output);
-	} else if (strcmp(argv[3],"-xor")==0) {
+	} else if (strcmp(argv[2],"-xor")==0) {
 		load_xor(values,output);
 	} else {
 		imprimir_uso();
@@ -163,9 +171,9 @@ int main(int argc, char ** argv){
 	}
 
 	if (strcmp(argv[1], "-a")==0){
-		adaline(n, 0.01, values, output);
+		adaline(2, 0.01, values, output);
 	} else if (strcmp(argv[1], "-p")==0){
-		perceptron(n, 0.5, 0.01, values, output);
+		perceptron(2, 0.5, 0.01, values, output);
 	}
 
 }
