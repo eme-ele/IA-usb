f31b5f172353c672e185377f64a00346eeb5d918
warles34 <warles34@gmail.com>
2013-03-10 12:20:35 -0430

    Ultimo estado gabil encode

--- p3/encode.py
+++ p3/encode.py
@@ -1,79 +1,5 @@
 import random
-from gabil import *
 
-# En esta funcion entra el numero binario sin el antecedente "0b" que llevan normalmente los resultantes de la funcion bin()
-def complete_bin(size,binario):
-	original_size = len(binario)
-	if original_size == size:
-		return binario
-	for x in range(size - original_size) :
-		binario = '0' + binario
-	return binario
-
-
-
-
-def continue_clasification(lim_inf, lim_sup, div, value):
-	size = len(bin(div)[1:])
-	step = (lim_sup - lim_inf)/float(div)
-	#print div
-	if value == "?":
-		return "1"*size
-	for x in range(1,2**size):
-		#print "value: " + str(value) + "< limite inferior + step*x" + str(lim_inf + step*x)
-		
-		next = lim_inf+step*x
-		if float(value) < float(next):
-			return complete_bin(size,bin(x-1)[2:])
-
-def continue_reversion(lim_inf, lim_sup, div, value):
-	print " " + str(value)
-	if value[0] == "1":
-		return "?"
-	part = int(value,2)
-	step = (lim_sup - lim_inf)/float(div)
-	return round(lim_inf + step*part,2)
-
-
-
-def bin_to_list(binary,datos):
-	lista = []
-	elem = ""
-	for x in range(16):
-		if dictionary[x]:
-			#print "dic in A" + str(x+1)
-			elem = binary[:len(dictionary[x].items()[0][1])]
-			binary = binary[len(dictionary[x].items()[0][1]):]
-		else:
-			#print "con in          A" + str(x+1)
-			elem = binary[:len(bin(datos[x][2])[1:])]
-			binary = binary[len(bin(datos[x][2])[1:]):]
-		lista.append(elem)
-	lista.append(binary)
-	
-	return lista
-
-
-def mask_matrix(bin_rule,datos):
-	lista = bin_to_list(bin_rule,datos)
-	#print len(bin_rule)
-	c = 0
-	for l in lista:
-		c += len(l)
-	#print c
-	#print c
-	matrix = []
-	for x in range(16):
-		new = ""
-		for y in range(16):
-			if x==y:
-				new += '1'*len(lista[y])
-			else:
-				new += '0'*len(lista[y])
-		matrix.append(new)
-	#print len(matrix[0])
-	exit(-1)
-	return matrix
 
 #DICCIONARIO DE TERMINOS
 
@@ -88,6 +14,21 @@ A12 = dict({"t":"00", "f":"01", "?" : "10"})
 A13 = dict({"g":"00", "p":"01", "s":"10", "?" : "11"})
 A16 = dict({"-":"00", "+":"01", "?" : "10"})
 dictionary = [A1,0,0,A4,A5,A6,A7,0,A9,A10,0,A12,A13,0,0,A16]
+
+
+#####ENCODING#####
+
+
+def continue_clasification(lim_inf, lim_sup, div, value):
+	size = len(bin(div)[1:])
+	step = (lim_sup - lim_inf)/float(div)
+	if value == "?":
+		return "1"*size
+	for x in range(1,2**size):
+		next = lim_inf+step*x
+		if float(value) < float(next):
+			return complete_bin(size,bin(x-1)[2:])
+
 
 def encode(features, data):
 	if len(features)!=16:
@@ -111,26 +52,43 @@ def encode_population(population,data):
 	return bin_pop
 
 
+#####DECODING#####
+
+def continue_reversion(lim_inf, lim_sup, div, value):
+	if value[0] == "1":
+		return "?"
+	part = int(value,2)
+	step = (lim_sup - lim_inf)/float(div)
+	return round(lim_inf + step*part,2)
+
+
 
 def decode(features,data):
 	lista = []
 	features =  bin_to_list(features,data)
+	print features
 	for x in range(16):
 		if dictionary[x]:
 			for key,value in dictionary[x].items():
 				if value == features[x]:
-					
 					lista.append(key)
 					break
 		else:
 			lista.append(continue_reversion(data[x][0],data[x][1],data[x][2],features[x]))
+		print lista
+	exit(-1)
 	return lista
+
 
 def decode_population(population,data):
 	feat_pop = []
 	for p in population:
 		feat_pop.append(decode(p,data))
 	return feat_pop
+
+
+
+#####UTILITIES#####
 
 
 def random_binary(tam):
@@ -143,24 +101,69 @@ def random_binary(tam):
 	return res
 
 
+# En esta funcion entra el numero binario sin el antecedente "0b" que llevan normalmente los resultantes de la funcion bin()
+def complete_bin(size,binario):
+	original_size = len(binario)
+	if original_size == size:
+		return binario
+	for x in range(size - original_size) :
+		binario = '0' + binario
+	return binario
+
+
+
+def bin_to_list(binary,datos):
+	lista = []
+	elem = ""
+	for x in range(16):
+		if dictionary[x]:
+			elem = binary[:len(dictionary[x].items()[0][1])]
+			binary = binary[len(dictionary[x].items()[0][1]):]
+		else:
+			elem = binary[:len(bin(datos[x][2])[1:])]
+			binary = binary[len(bin(datos[x][2])[1:]):]
+		lista.append(elem)
+	return lista
+
+
+
+def mask_matrix(bin_rule,datos):
+	lista = bin_to_list(bin_rule,datos)
+	c = 0
+	matrix = []
+	for x in range(15):
+		new = ""
+		for y in range(16):
+			if x==y:
+				new += '1'*len(lista[y])
+			else:
+				new += '0'*len(lista[y])
+		matrix.append(new)
+	return matrix
+
+
+
+
+
+
+
+
 def test():
-	ejemplo = ['b',41.92,0.42,'u','g','c','h',0.21,'t','t',6,'f','g',220,948,'+']
-	datos = [0,[30,60,3000],[0,1,1000],0,0,0,0,[0,1,1000],0,0,[0,10,2000],0,0,[200,300,1000],[900,1000,10000],0]
+	ejemplo = ['b','41.92','0.42','u','g','c','h','0.21','t','t',6,'f','g','220','948','+']
+	datos = [0,[30,60,30],[0,1,100],0,0,0,0,[0,1,10],0,0,[0,10,200],0,0,[200,300,10],[900,1000,26],0]
 	print ejemplo
 	binary = encode(ejemplo,datos)
-	#binary = random_binary(len(binary))
+	binary = random_binary(len(binary))
 	print binary
 	print len(binary)
-	
 	features = bin_to_list(binary,datos)
 	print features
+
 	print decode(binary,datos)
-	print mutation("000111000111000111")
-	print  mask_matrix(binary,datos)
-	print "hola\n\n\n\n\n\n\n"
-	for x in m:
-		print x
-		print "hola"
+	#print mutation("000111000111000111")
+	m = mask_matrix(binary,datos)
+
+
 
 
 
--- p3/gabil.py
+++ p3/gabil.py
@@ -3,11 +3,6 @@ import random
 import math
 from encode import *
 
-def parse_file(file_name):
-	pass
-
-def encode(example):
-	pass
 
 def complete_bin(size,binario):
 	original_size = len(binario)
@@ -58,28 +53,14 @@ def probabilidades(P, total_fitness, fitness_list):
 def weel_select(P, n, total_fitness, fitness_list):
 	PS = []
 	Pr = probabilidades(P, total_fitness, fitness_list)
-	#print P
-	#print total_fitness
-	#print fitness_list
-	#print Pr
-	#print "\n\n\n\n"
 	for i in range(n):
-
 		lanzamiento = random.random()
 		circle = 0
-		#print len(PS)
-		#print "El lanzamiento fue de:" + str(lanzamiento)
-		#print i
 		for e in range(len(Pr)):
 			circle += Pr[e][0]
-			#print " 	Es menor que " + str(circle) + " ?"
 			if lanzamiento <= circle:
 				PS.append(P[Pr[e][1]])
-				#Pr.remove(Pr[e])
 				break
-			#print " 	No por ahora"
-	#print PS
-	#exit(-1)
 	return PS			
 
 
@@ -110,8 +91,6 @@ def crossover(individuo1,individuo2):
 	while point_4 <= point_3:
 		point_3 = [random.randint(0,len(individuo2)/rule_size-1),point_1[1]]
 		point_4 = [random.randint(0,len(individuo2)/rule_size-1),point_2[1]]
-	#print [point_1,point_2]
-	#print [point_3,point_4]
 	son1 = individuo1[:point_1[0]*rule_size+point_1[1]] + individuo2[point_3[0]*rule_size+point_3[1]:point_4[0]*rule_size+point_4[1]] + individuo1[point_2[0]*rule_size+point_2[1]:] 
 	son2 = individuo2[:point_3[0]*rule_size+point_3[1]] + individuo1[point_1[0]*rule_size+point_1[1]:point_2[0]*rule_size+point_2[1]] + individuo2[point_4[0]*rule_size+point_4[1]:] 
 	return [son1,son2]
@@ -203,9 +182,9 @@ def GA(ejemplos, p, r, m):
 		PS = drop_cond(PS)
 		#print "drop_cond con 0.6" + str(PS)
 		P = PS
-		print "P: " + str(P)
+		#print "P: " + str(P)
 		fitness_list = compute_fitness(P, ejemplos)
-		print "fitness: " + str(fitness_list)
+		print "fitness: " + str(max(fitness_list))
 		iter += 1
 
 		
@@ -255,18 +234,18 @@ def main():
 	#ejemplos = encode(opts.file_name)
 	global rule_size, mask_atributos
 	# luego hay que cambiarlos para los ejemplos dados
-
 	population = read_population(opts.file_name)
 	#data debe crearse con un vector de numero de intervalos... cada uno representaria el numero de divisiones del elemento i del rasgo continuo
-	data = create_data(population,[1000,1000,1000,1000,1000,10000])
-	print population[0]
+	div = int(math.ceil(math.sqrt(len(population))))
+	data = create_data(population,[div,div,div,div,div,div])
+	#print population[0]
 	ejemplos = encode_population(population,data)
 	rule_size = len(ejemplos[0])
-	print ejemplos[0]
-	print rule_size
-	
-	mask_atributos = ['11000', '00110']
-
+	#print ejemplos[0]
+	#print len(ejemplos[0])
+	mask_atributos = mask_matrix(ejemplos[0],data)
+	#print len(mask_atributos[0])
+	#print mask_atributos
 	back_population = decode_population(ejemplos,data)
 	GA(ejemplos, opts.p, opts.r, opts.m)
 	
