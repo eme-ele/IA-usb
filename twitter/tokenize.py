820a9eb717e0b2e9650821ac64579df63350879b
ML <emele18@gmail.com>
2013-04-03 20:13:28 -0430

    data tokenizada + programa usado para tokenizar

---
+++ twitter/tokenize.py
@@ -0,0 +1,20 @@
+""" Wrapper for twitter tokenizer. See README for usage instruction"""
+"""Kevin Gimpel (kgimpel@cs.cmu.edu)"""
+
+import sys,os
+import lib.twokenize
+
+def tokenize(istring,ostring):
+  ifile=open(istring,'r')
+  ofile=open(ostring,'w')
+  for line in ifile:
+    ofile.write(u" ".join(lib.twokenize.tokenize(line[:-1])).encode('utf-8')+'\n') 
+  ofile.close()
+  ifile.close()
+
+if __name__=='__main__':
+  try:
+    tokenize(*sys.argv[1:])
+  except TypeError: 
+    print "Usage : python tokenize.py <input file> <output file>"
+    print "See README for input file format"
